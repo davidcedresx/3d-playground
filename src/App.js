304309d1ef9601440bc66bcc86a35e1fe9817a0c
David Cedres <david.cedresx@gmail.com>
2021-02-13 16:34:01 -0400

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import * as THREE from "three";
-export default class App extends React.Component {
-  componentDidMount() {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    this.mount.appendChild(renderer.domElement);
 
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshStandardMaterial({ color: 0x7e31eb });
-    var cube = new THREE.Mesh(geometry, material);
+export default function Playground() {
+  const node = useRef(null)
+
+  useEffect(() => {
+    // main instances
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+
+    // setup renderer
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    node.current.appendChild(renderer.domElement);
+
+    // create cube
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial({ color: 0x7e31eb });
+    const cube = new THREE.Mesh(geometry, material);
     scene.add(cube);
+
+    // setup some lighting
     const light = new THREE.HemisphereLight(0xffffbb, 0x080820, 1);
     scene.add(light);
+
+    // setup camera
     camera.position.z = 2;
-    var animate = function () {
+
+    // animation loop
+    const animate = function () {
       requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
@@ -23,10 +36,10 @@ export default class App extends React.Component {
       renderer.render(scene, camera);
     };
     animate();
-  }
-  render() {
-    return (
-      <div ref={ref => (this.mount = ref)} />
-    )
-  }
+  }, [])
+
+
+  return (
+    <div ref={node} />
+  )
 }
